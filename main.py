ad4f2c25cda2b8d9f114e58306056414f45d33ce
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-07-01 22:00:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import torch
+import kagglehub
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+kagglehub.login()
+
+model_path = kagglehub.model_download("google/gemma-3n/transformers/gemma-3n-e2b-it")
