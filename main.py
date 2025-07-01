8cf33c6eaac741064a4efa7a9328a5c0248545b1
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-07-01 23:23:24 +0530

    testing the prompt with gemma

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import torch
 import kagglehub
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
 
 kagglehub.login()
 
 model_path = kagglehub.model_download("google/gemma-3n/transformers/gemma-3n-e2b")
+
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True)
+
+prompt = "how many digits does googol have"
+
+inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+generation_config = GenerationConfig(
+    max_new_tokens=150, do_sample=True, temperature=0.7
+)
+outputs = model.generate(**inputs, generation_config=generation_config)
+result = tokenizer.decode(outputs[0], skip_special_tokens=True)
